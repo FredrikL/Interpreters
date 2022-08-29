178dde6fead0e72e4511873a8193979142f2bbf9
Fredrik Leijon <fredrik.leijon@gmail.com>
2022-08-29 19:56:54 +0200

    Fields

--- com/craftinginterpreters/lox/Interperter.java
+++ com/craftinginterpreters/lox/Interperter.java
@@ -51,6 +51,17 @@ class Interperter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Object visitSetExpr(Expr.Set expr) {
+    Object object = evaluate(expr.object);
+    if (!(object instanceof LoxInstance)) {
+      throw new RuntimeError(expr.name, "Only instances have fields.");
+    }
+    Object value = evaluate(expr.value);
+    ((LoxInstance) object).set(expr.name, value);
+    return value;
+  }
+
+  @Override
   public Object visitCallExpr(Expr.Call expr) {
     Object callee = evaluate(expr.callee);
 
@@ -69,6 +80,15 @@ class Interperter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
           "Expected " + function.arity() + " arguments but got " + arguments.size() + ".");
     }
     return function.call(this, arguments);
+  }
+
+  @Override
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (object instanceof LoxInstance) {
+      return ((LoxInstance) object).get(expr.name);
+    }
+    throw new RuntimeError(expr.name, "Only instances have properties.");
   }
 
   @Override
@@ -175,6 +195,14 @@ class Interperter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   @Override
   public Void visitBlockStmt(Stmt.Block stmt) {
     executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
+
+  @Override
+  public Void visitClassStmt(Stmt.Class stmt) {
+    environment.define(stmt.name.lexeme, null);
+    LoxClass klass = new LoxClass(stmt.name.lexeme);
+    environment.assign(stmt.name, klass);
     return null;
   }
 
