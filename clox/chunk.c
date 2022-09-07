7c9991ab837ba0a322895cab24f10ea03f768ee0
Fredrik Leijon <fredrik.leijon@gmail.com>
2022-09-07 19:35:22 +0200

    Chunk

---
+++ clox/chunk.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include "chunk.h"
+#include "memory.h"
+
+void initChunk(Chunk *chunk)
+{
+  chunk->count = 0;
+  chunk->capacity = 0;
+  chunk->code = NULL;
+}
+
+void freeChunk(Chunk *chunk)
+{
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  initChunk(chunk);
+}
+
+void writeChunk(Chunk *chunk, uint8_t byte)
+{
+  if (chunk->capacity < chunk->count + 1)
+  {
+    int oldCapcity = chunk->capacity;
+    chunk->capacity = GROW_CAPACITY(oldCapcity);
+    chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapcity, chunk->capacity);
+  }
+  chunk->code[chunk->count] = byte;
+  chunk->count++;
+}
