46430532c612f5cb31654433992bc9660958d724
Fredrik Leijon <fredrik.leijon@gmail.com>
2022-11-26 13:59:27 +0100

    Loops, c23

--- clox/vm.c
+++ clox/vm.c
@@ -247,6 +247,12 @@ static InterpretResult run()
         vm.ip += offset;
       break;
     }
+    case OP_LOOP:
+    {
+      uint16_t offset = READ_SHORT();
+      vm.ip -= offset;
+      break;
+    }
     case OP_RETURN:
       // Exit interperter.
       return INTERPRET_OK;
