12779b1111455c4a2e3cfab0c1cebd43bfbea1f7
Fredrik Leijon <fredrik.leijon@gmail.com>
2022-12-30 16:09:16 +0100

    Chapter 30

--- clox/table.c
+++ clox/table.c
@@ -23,7 +23,7 @@ void freeTable(Table *table)
 
 static Entry *findEntry(Entry *entires, int capacity, ObjString *key)
 {
-  uint32_t index = key->hash % capacity;
+  uint32_t index = key->hash & (capacity - 1);
   Entry *tombstone = NULL;
   for (;;)
   {
@@ -47,7 +47,7 @@ static Entry *findEntry(Entry *entires, int capacity, ObjString *key)
       return entry;
     }
 
-    index = (index + 1) % capacity;
+    index = (index + 1) & (capacity - 1);
   }
 }
 
@@ -139,7 +139,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
   if (table->count == 0)
     return NULL;
 
-  uint32_t index = hash % table->capacity;
+  uint32_t index = hash & (table->capacity - 1);
   for (;;)
   {
     Entry *entry = &table->entries[index];
@@ -154,7 +154,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t
       // We found it.
       return entry->key;
     }
-    index = (index + 1) % table->capacity;
+    index = (index + 1) & (table->capacity - 1);
   }
 }
 
